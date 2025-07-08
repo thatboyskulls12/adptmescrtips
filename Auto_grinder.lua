a8101dd02d04d5c7ec989290205ad9d25a09e4f4
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-08 18:40:29 -0400

    Create Auto_grinder.lua

---
+++ Auto_grinder.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/gui_loader.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/source.lua"))()
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c29af06acdb7a80fb239415fd322efe9.lua"))()
