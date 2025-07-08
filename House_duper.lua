a6f29020e4a178e24a9d7c4cc1c5dddd798b2db8
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-08 18:53:24 -0400

    Update House_duper.lua

--- House_duper.lua
+++ House_duper.lua
@@ -1,5 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/gui_loader.lua"))()
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/source_1.lua"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/source.lua"))()
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a1785d95674fad4fef000612f70f2608.lua"))()
